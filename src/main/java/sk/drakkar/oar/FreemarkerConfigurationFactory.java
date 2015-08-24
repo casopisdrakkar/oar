2e09fc1277f81e228d0cf3f5550409126262b123
novotnyr <robert.novotny@upjs.sk>
2015-08-24 09:59:02 +0200

    Enable Freemarker API built-ins for non string hashes

--- src/main/java/sk/drakkar/oar/FreemarkerConfigurationFactory.java
+++ src/main/java/sk/drakkar/oar/FreemarkerConfigurationFactory.java
@@ -10,12 +10,14 @@ public enum FreemarkerConfigurationFactory {
 
 	public Configuration getConfiguration() {
 		if(this.freemarkerConfiguration == null) {		
-			freemarkerConfiguration = new Configuration(Configuration.VERSION_2_3_21);
+			freemarkerConfiguration = new Configuration(Configuration.VERSION_2_3_23);
 
 			freemarkerConfiguration.setClassForTemplateLoading(Oar.class, "");
 			freemarkerConfiguration.setDefaultEncoding("UTF-8");
 
 			freemarkerConfiguration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+			freemarkerConfiguration.setAPIBuiltinEnabled(true);
 		} 
 		return freemarkerConfiguration;
 	}
